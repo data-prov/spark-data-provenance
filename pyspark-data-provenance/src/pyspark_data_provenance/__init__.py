7fc95d493c3452ae560a0ed9a948a6ff13511c4f
Denis Arnaud <denis.arnaud@decathlon.com>
2026-03-12 02:01:48 +0100

    [pyspark] Fixed linter issue

--- pyspark-data-provenance/src/pyspark_data_provenance/__init__.py
+++ pyspark-data-provenance/src/pyspark_data_provenance/__init__.py
@@ -1,5 +1,7 @@
 from .data_provenance import (
     build_data_provenance_session as build_data_provenance_session,
+)
+from .data_provenance import (
     data_provenance_enabled as data_provenance_enabled,
 )
 
