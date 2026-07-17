5cf6dfeeda2e1fef918f1878deaeb51f30ae7165
Anais Ballard <anais.ballard@decathlon.com>
2026-07-17 16:59:59 +0200

    feat: modify provenance extracor and python part

--- scala-spark-data-provenance/src/main/scala/org/dataprov/dp/sparkdataprovenance/ProvenanceExtractor.scala
+++ scala-spark-data-provenance/src/main/scala/org/dataprov/dp/sparkdataprovenance/ProvenanceExtractor.scala
@@ -5,70 +5,59 @@ import org.apache.spark.sql.functions.col
 import org.apache.spark.sql.functions.explode
 import org.apache.spark.sql.types.ArrayType
 
+import scala.jdk.CollectionConverters._
+
 object ProvenanceExtractor {
 
   /** Extract the minimal datasets from the source DataFrames that are necessary
     * to reproduce the final DataFrame. This is useful for debugging and
     * understanding the lineage of specific rows.
-    *
-    * @param finalDF
-    *   The final tagged DataFrame from the pipeline
-    * @param sourceDFs
-    *   The list of original source DataFrames (must have their ID/UUID column)
-    * @param provenanceColName
-    *   The name of the provenance column (e.g., "_provenance_tag")
-    * @return
-    *   A list of DataFrames containing a set of rows necessary to reproduce the
-    *   final DataFrame
     */
-  def extractMinimalDatasets(
+  def getMinimalSources(
       finalDF: DataFrame,
-      sourceDFs: Seq[DataFrame],
-      provenanceColName: String = "_provenance_tag"
-  ): Seq[DataFrame] = {
+      sourceDFs: java.util.List[DataFrame],
+      provenanceColName: String
+  ): java.util.List[DataFrame] = {
 
-    val spark = finalDF.sparkSession
-    import spark.implicits._
+    val sparkSession = finalDF.sparkSession
+    val wasEnabled =
+      sparkSession.conf.get("spark.provenance.enabled", "false") == "true"
 
-    // 1.Determine the schema of the provenance column to understand its structure (Array, String, etc.)
-    val provField = finalDF.schema(provenanceColName)
+    try {
+      sparkSession.conf.set("spark.provenance.enabled", "false")
 
-    // 2. Extract and flatten all unique tags present in the final result
-    val targetTagsDF = finalDF.select(col(provenanceColName))
+      // 1. Determine the schema of the provenance column to understand its structure (Array, String, etc.)
+      val provField = finalDF.schema(provenanceColName)
 
-    val flattenedTagsDF = provField.dataType match {
-      // Semi-Why case: Array[Tag] (e.g., Array[String] or Array[Long])
-      case _: ArrayType =>
-        targetTagsDF.select(explode(col(provenanceColName)).as("source_tag"))
+      // 2. Extract and flatten all unique tags present in the final result
+      val targetTagsDF = finalDF.select(col(provenanceColName))
 
-      // Boolean / Display / Simple Id case: Direct scalar value
-      case _ =>
-        targetTagsDF.withColumnRenamed(provenanceColName, "source_tag")
-    }
+      val finalTags = provField.dataType match {
+        // Semi-Why case: Array[Tag] (e.g., Array[String] or Array[Long])
+        case _: ArrayType =>
+          targetTagsDF
+            .select(explode(col(provenanceColName)).as(provenanceColName))
+            .distinct()
 
-    // We collect the unique tags into a Set for efficient filtering.
-    // This is especially useful when dealing with large datasets.
-    // Ideal for debugging a few target rows
-    val activeSourceTags = flattenedTagsDF
-      .distinct()
-      .as[String]
-      .collect()
-      .toSet
+        // Boolean / Display / Simple Id case: Direct scalar value
+        case _ =>
+          targetTagsDF.distinct()
+      }
 
-    // 3. Filter each source DataFrame to retain only the rows that match the active tags
-    sourceDFs.map { sourceDF =>
-      // We look for the column that serves as the identifier in the source.
-      // In your framework, this is often the generated UUID or the original primary key.
-      val idColumnName =
-        if (sourceDF.columns.contains(provenanceColName)) provenanceColName
-        else "id"
+      // 3. Filter each source DataFrame to retain only the rows that match the active tags using Left Semi Join
+      val filteredSources = sourceDFs.asScala.map { sourceDF =>
+        if (sourceDF.columns.contains(provenanceColName)) {
+          sourceDF.join(finalTags, Seq(provenanceColName), "left_semi")
+        } else {
+          sourceDF
+        }
+      }
 
-      // Safety in case the source has neither
-      if (sourceDF.columns.contains(idColumnName)) {
-        sourceDF.filter(col(idColumnName).isin(activeSourceTags.toSeq: _*))
-      } else {
-        // If no tracking column is found, return the original DF intact
-        sourceDF
+      filteredSources.asJava
+
+    } finally {
+      if (wasEnabled) {
+        sparkSession.conf.set("spark.provenance.enabled", "true")
       }
     }
   }
